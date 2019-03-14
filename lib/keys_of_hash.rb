82d89f59a367ca200eb0755dfc01035e40d23253
Jeff M <jkdmarks@gmail.com>
2019-03-14 02:04:52 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,13 +1,15 @@
 class Hash
   def keys_of(*arguments)
-    if arguments.include?("Panama")
-      ["red-footed tortoise"]
-    elsif arguments.include?("Madagascar")
-      ["aye-aye", "tomato frog"]
-    elsif arguments.include?("Australia")
-      ["sugar glider", "kangaroo", "koala"]
-    elsif arguments.class == Symbol
-      arguments
-    end
+    arguments.keys
   end
 end
+
+# if arguments.include?("Panama")
+#   ["red-footed tortoise"]
+# elsif arguments.include?("Madagascar")
+#   ["aye-aye", "tomato frog"]
+# elsif arguments.include?("Australia")
+#   ["sugar glider", "kangaroo", "koala"]
+# elsif arguments.class == Symbol
+#   arguments
+# end
