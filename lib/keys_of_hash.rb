109fd3d9611d97653dfaaff3a7a7bd062cc6d3ce
Jeff M <jkdmarks@gmail.com>
2019-03-14 02:01:43 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
-    if arguments.includ?("Panama")
+    if arguments.include?("Panama")
       ["red-footed tortoise"]
     elsif arguments == "Madagascar"
       ["aye-aye", "tomato frog"]
